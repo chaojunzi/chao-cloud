68bb1ff7d36e506dc8d23a64de43ddbf13bd80ba
xuechao <1521515935@qq.com>
2020-10-15 15:03:33 +0800

    增加分页获取list

--- chao-cloud-common/chao-cloud-common-extra/src/main/java/com/chao/cloud/common/extra/mybatis/util/MybatisUtil.java
+++ chao-cloud-common/chao-cloud-common-extra/src/main/java/com/chao/cloud/common/extra/mybatis/util/MybatisUtil.java
@@ -1,5 +1,6 @@
 package com.chao.cloud.common.extra.mybatis.util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -7,6 +8,8 @@ import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.chao.cloud.common.extra.mybatis.constant.MybatisConstant;
+
+import cn.hutool.core.collection.CollUtil;
 
 /**
  * mybatis 工具
@@ -55,5 +58,51 @@ public interface MybatisUtil {
 		if (page.hasNext()) {// 查询下一页并处理
 			forEachPageRecursion(current + 1, size, service, wrapper, action);
 		}
+	}
+
+	/**
+	 * list分页递归
+	 * 
+	 * @param <T>     数据实体
+	 * @param current 当前页
+	 * @param service 实体对应的service层
+	 * @param wrapper 封装条件
+	 * @param result  返回的list
+	 * @return result
+	 */
+	public static <T> List<T> listPageRecursion(long current, IService<T> service, Wrapper<T> wrapper, List<T> result) {
+		return listPageRecursion(current, MybatisConstant.SIZE, service, wrapper, null);
+	}
+
+	/**
+	 * list分页递归
+	 * 
+	 * @param <T>     数据实体
+	 * @param current 当前页
+	 * @param size    每页的数量
+	 * @param service 实体对应的service层
+	 * @param wrapper 封装条件
+	 * @param result  返回的list
+	 * @return result
+	 */
+	public static <T> List<T> listPageRecursion(long current, long size, IService<T> service, Wrapper<T> wrapper,
+			List<T> result) {
+		if (result == null) {
+			result = new ArrayList<T>();
+		}
+		// 分页对象
+		Page<T> page = new Page<>(current, size);
+		// 查询
+		service.page(page, wrapper);
+		// 获取数据并处理
+		List<T> list = page.getRecords();
+		if (CollUtil.isNotEmpty(list)) {
+			result.addAll(list);
+		}
+		// 判断是否有下一页
+		if (page.hasNext()) {// 查询下一页并处理
+			listPageRecursion(current + 1, size, service, wrapper, result);
+		}
+		return result;
 	}
 }
